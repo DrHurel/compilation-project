8da167da28d06a8502309ff21028a42701431e29
Hurel <jeremyhurel34@gmail.com>
2024-11-28 10:06:26 +0100

    save

--- testing/my-fibo.lisp
+++ testing/my-fibo.lisp
@@ -1,14 +1,9 @@
 (defun fibo (n)
-    (if (< n 0)
-        (error "Not a positive int")
+    (cond 
+        (< n 0)(error "Not a positive int")
+        (= n 0) n
+        (= n 1) n
+        (t) (fibo (n-2))+ (fibo (n-1))
     )
-    (when (= n 0)
-      n
-    )
-    (when (= n 1) 
-        n
-    )
-
-    ((fibo (n-2))+ (fibo (n-1)))
 
 )
