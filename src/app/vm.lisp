a2410a41a5aedc97eb63fae54679e31befd01d01
Hurel <jeremyhurel34@gmail.com>
2025-01-05 16:34:56 +0100

    wip

--- src/app/vm.lisp
+++ src/app/vm.lisp
@@ -1,19 +1,88 @@
 ;; Load the module manually
-(load "src/utils/assembly.lisp")
-(load "src/utils/vm-functions.lisp")
+(require "src/utils/assembly.lisp")
+(require "src/utils/vm-functions.lisp")
 
-;; Main function
-(defun main ()
-  (say-hello)
-  (setq "is_running" T)
-  (setq "instruction" nil)
-  (print_start_info)
 
-  (loop while is_running do (
-    (fetch instruction)
-    (if (evaluate instruction)
-    (run instruction is_running))
-    )
-  )
-  
-)
+(defun vm-load (vm program)
+    ;; Détermine l'adresse de départ pour charger le programme
+  (let ((initial-pc (- (or (var-basse-get vm +last-code-id+) (+ (pc-get vm) 1)) 1)))
+        ;; Charge les instructions et les labels
+        (loop for instruction in program do
+            (if (is-label instruction)
+                ;; Si c'est un label, stocke son adresse dans la table des labels
+                (etiq-set vm (string (second instruction)) initial-pc)
+                ;; Sinon, stocke l'instruction en mémoire et met à jour initial-pc
+                (progn
+                    (mem-set vm initial-pc instruction)
+                    (setq initial-pc (- initial-pc 1)))))
+
+        ;; Met à jour :LAST_CODE
+        (var-basse-set vm +last-code-id+ (+ initial-pc 1))
+
+        ;; Mise à jour des adresses pour les sauts
+        (update-labels-for-jumps vm)))
+
+(defun vm-execute (vm)
+  (loop while (and (>= (pc-get vm) (var-basse-get vm +last-code-id+)) (is-running vm)) do
+    (let ((instruction (mem-get vm (pc-get vm))))
+      (when (is-debug vm) (format t "~A " instruction))
+      (match (first instruction)
+        (('LOAD) (vm-LOAD vm (second instruction) (third instruction)))
+        (('STORE) (vm-STclearORE vm (second instruction) (third instruction)))
+        (('MOVE) (vm-MOVE vm (second instruction) (third instruction)))
+        (('ADD) (vm-ADD vm (second instruction) (third instruction)))
+        (('SUB) (vm-SUV vm (second instruction) (third instruction)))
+        (('MUL) (vm-MUL vm (second instruction) (third instruction)))
+        (('DIV) (vm-DIV vm (second instruction) (third instruction)))
+        (('INCR) (vm-INCR vm (second instruction)))
+        (('DECR) (vm-DECR vm (second instruction)))
+        (('PUSH) (vm-PUSH vm (second instruction)))
+        (('POP) (vm-POP vm (second instruction)))
+        (('JMP) (vm-JMP vm (second instruction)))
+        (('CMP) (vm-CMP vm (second instruction) (third instruction)))
+        (('JSR) (vm-JSR vm (second instruction)))
+        (('JGT) (vm-JGT vm (second instruction)))
+        (('JGE) (vm-JGE vm (second instruction)))
+        (('JLT) (vm-JLT vm (second instruction)))
+        (('JLE) (vm-JLE vm (second instruction)))
+        (('JEQ) (vm-JEQ vm (second instruction)))
+        (('JNE) (vm-JNE vm (second instruction)))
+        (('TEST) (vm-TEST vm (second instruction)))
+        (('JNIL) (vm-JNIL vm (second instruction)))
+        (('JTRUE) (vm-JTRUE vm (second instruction)))
+        (('NOP) (vm-NOP vm))
+        (('HALT) (vm-HALT vm))
+        (_
+          (format t "Unknown instruction: ~A~%" instruction)
+        )
+      )
+      (pc-decr vm)
+      (when (is-debug vm)
+        (format t "R0: ~A R1: ~A R2: ~A SP: ~A FP: ~A Stack: ~A~%"
+                (attr-get vm :R0)
+                (attr-get vm :R1)
+                (attr-get vm :R2)
+                (attr-get vm :SP)
+                (attr-get vm :FP)
+                (stack-get vm))))))
+
+
+(defun vm-reset(vm &optional (size 1000))
+  (let ((size (max size 1000)) (variablesBasse 30) (tailleZones (- (max size 1000) 30)))
+    (attr-set vm :R0 0)
+    (attr-set vm :R1 0)
+    (attr-set vm :R2 0)
+    (attr-set vm :MAX_MEM size)          ;; Définition de la taille de la VM
+    (attr-array-init vm :MEM size)       ;; Définition de la mémoire
+    (var-basse-set vm +start-code-id+ (- size 1))
+    (var-basse-set vm +etiq-id+ (make-hash-table))
+    (pc-set vm (- size 1))               ;; puis on va diminuer dans la mémoire, ça permet de ne pas trop se faire de soucis
+    (bp-set vm 30)                       ;; Le BP lui est défini après les variables basses.
+    (sp-set vm (bp-get vm))              ;; Le stack pointer est de base sur BP.
+    (fp-set vm (sp-get vm))
+    (ms-set vm (+ variablesBasse (/ tailleZones 2))) ;; s'en suit la valeur maximum du stack qu'on ne doit pas dépasser
+    (set-running vm 1)))                 ;; Ainsi pour une VM taille 1000: BP = 30, SP = 30, MS = 224, MAX_MEM = 1000
+
+(defun vm-init(vm &optional (size 1000))
+  (attr-set vm :NAME vm)
+  (vm-reset vm))
--- src/utils/assembly.lisp
+++ src/utils/assembly.lisp
@@ -1,45 +1,45 @@
 ;; (LOAD <src> <dest>) chargement de mémoire à registre
-(defun vm-LOAD (src dest)
+(defun vm-LOAD (vm src dest)
   `(progn (setf ,dest (read-from-memory ,src))))
 
 ;; (STORE <src> <dest>) chargement de registre à mémoire
-(defun vm-STORE (src dest)
+(defun vm-STORE (vm src dest)
   `(progn (write-to-memory ,dest ,src)))
 
 ;; (MOVE <src> <dest>) mouvement de registre à registre
-(defun vm-MOVE (src dest)
+(defun vm-MOVE (vm src dest)
   `(setf ,dest ,src))
 
 ;; (ADD <src> <dest>) addition
-(defun vm-ADD (src dest)
+(defun vm-ADD (vm src dest)
   `(setf ,dest (+ ,dest ,src)))
 
 ;; (SUB <src> <dest>) soustraction
-(defun vm-SUB (src dest)
+(defun vm-SUB (vm src dest)
   `(setf ,dest (- ,dest ,src)))
 
 ;; (MUL <src> <dest>) multiplication
-(defun vm-MUL (src dest)
+(defun vm-MUL (vm src dest)
   `(setf ,dest (* ,dest ,src)))
 
 ;; (DIV <src> <dest>) division
-(defun vm-DIV (src dest)
+(defun vm-DIV (vm src dest)
   `(setf ,dest (/ ,dest ,src)))
 
 ;; (INCR <dest>) incrément
-(defun vm-INCR (dest)
+(defun vm-INCR (vm dest)
   `(setf ,dest (1+ ,dest)))
 
 ;; (DECR <dest>) décrément
-(defun vm-DECR (dest)
+(defun vm-DECR (vm dest)
   `(setf ,dest (1- ,dest)))
 
 ;; (PUSH <src>) empiler
-(defun vm-PUSH (src)
+(defun vm-PUSH (vm src)
   `(push ,src *stack*))
 
 ;; (POP <dest>) dépiler
-(defun vm-POP (dest)
+(defun vm-POP (vm dest)
   `(setf ,dest (pop *stack*)))
 
 ;; (LABEL <label>) déclaration d’étiquette
@@ -47,62 +47,62 @@
   `(progn ,@(declare-label label)))
 
 ;; (JMP <label>) saut inconditionnel à une étiquette
-(defun vm-JMP (label)
+(defun vm-JMP (vm label)
   `(go ,label))
 
 ;; (JSR <label>) saut avec retour
-(defun vm-JSR (label)
+(defun vm-JSR (vm label)
   `(progn (push *program-counter* *stack*)
           (go ,label)))
 
 ;; (RTN) retour
-(defun vm-RTN ()
+(defun vm-RTN (vm)
   `(setf *program-counter* (pop *stack*)))
 
 ;; (CMP <src1> <src2>) comparaison
-(defun vm-CMP (src1 src2)
+(defun vm-CMP (vm src1 src2)
   `(setf *compare-result* (compare ,src1 ,src2)))
 
 ;; (JGT <label>) saut si plus grand
-(defun vm-JGT (label)
+(defun vm-JGT (vm label)
   `(when (> *compare-result* 0) (go ,label)))
 
 ;; (JGE <label>) saut si plus grand ou égal
-(defun vm-JGE (label)
+(defun vm-JGE (vm label)
   `(when (>= *compare-result* 0) (go ,label)))
 
 ;; (JLT <label>) saut si plus petit
-(defun vm-JLT (label)
+(defun vm-JLT (vm label)
   `(when (< *compare-result* 0) (go ,label)))
 
 ;; (JLE <label>) saut si plus petit ou égal
-(defun vm-JLE (label)
+(defun vm-JLE (vm label)
   `(when (<= *compare-result* 0) (go ,label)))
 
 ;; (JEQ <label>) saut si égal
-(defun vm-JEQ (label)
+(defun vm-JEQ (vm label)
   `(when (= *compare-result* 0) (go ,label)))
 
 ;; (JNE <label>) saut si différent
-(defun vm-JNE (label)
+(defun vm-JNE (vm label)
   `(when (/= *compare-result* 0) (go ,label)))
 
 ;; (TEST <src>) comparaison à NIL
-(defun vm-TEST (src)
+(defun vm-TEST (vm src)
   `(setf *compare-result* (not (null ,src))))
 
 ;; (JTRUE <label>) saut si non-NIL
-(defun vm-JTRUE (label)
+(defun vm-JTRUE (vm label)
   `(when *compare-result* (go ,label)))
 
 ;; (JNIL <label>) saut si NIL
-(defun vm-JNIL (label)
+(defun vm-JNIL (vm label)
   `(when (null *compare-result*) (go ,label)))
 
 ;; (NOP) rien
-(defun vm-NOP ()
+(defun vm-NOP (vm)
   nil)
 
 ;; (HALT) arrêt
-(defun vm-HALT ()
+(defun vm-HALT (vm)
   `(error "Program halted"))
